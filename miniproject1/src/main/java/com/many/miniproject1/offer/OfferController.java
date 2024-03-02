1eb547f7f53ba48a8267a498c00ac712605b6ffc
LifeIsOne <didtmdgh258@gmail.com>
2024-03-02 19:32:52 +0900

    Yet...<( ￣^￣)(θ(

--- miniproject1/src/main/java/com/many/miniproject1/offer/OfferController.java
+++ miniproject1/src/main/java/com/many/miniproject1/offer/OfferController.java
@@ -9,14 +9,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequiredArgsConstructor
 public class OfferController {
     //개인 제안 관리
-    @GetMapping("/person/post")
+    @GetMapping("/person/offer")
     public String personPost() {
         return "person/offerEmails";
     }
 
     //    제안 받은 이메일 디테일 머스태치가 없는 것으로 추정됨. 찾으면 알려주시고 공유해주세요. 꼭이요!!!
-    @GetMapping("/person/post/detail/{id}")
+    @GetMapping("/person/offer/detail/{id}")
     public String personPostDetail(@PathVariable int id) {
         return "company/offerEmailDetail";
+    }
+
+    @GetMapping("/person/offerEmailForm/")
+    public String pers() {
+        return "company/offerEmailForm";
     }
 }
