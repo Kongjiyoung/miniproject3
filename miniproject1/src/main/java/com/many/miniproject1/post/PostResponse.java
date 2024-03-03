6d61ff3495f60d3cac2128e25767777ecba44a4a
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-03-03 20:16:33 +0900

    postDetail 내용 추가

--- miniproject1/src/main/java/com/many/miniproject1/post/PostResponse.java
+++ miniproject1/src/main/java/com/many/miniproject1/post/PostResponse.java
@@ -1,4 +1,24 @@
 package com.many.miniproject1.post;
 
+import lombok.Data;
+
+import java.sql.Timestamp;
+
 public class PostResponse {
+    @Data
+    public static class DetailDTO{
+        private Integer id;
+        private Integer companyId;
+        private String title;
+        private String career;
+        private String pay;
+        private String workCondition;
+        private String workStartTime;
+        private String workEndTime;
+        private String deadline;
+        private String task;
+        private String profile;
+        private String skill;
+        private String workingArea;
+    }
 }
