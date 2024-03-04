1f1abb3d539fb868d574f4618645d3bf9aab5e9c
hyeonnit <lhyeonnit@gmail.com>
2024-03-04 20:28:55 +0900

    마이페이지 필ì요

--- miniproject1/src/main/java/com/many/miniproject1/user/UserController.java
+++ miniproject1/src/main/java/com/many/miniproject1/user/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class UserController {
@@ -104,13 +106,17 @@ public class UserController {
 
     //회사 정보 및 수정
     //회사 정보 수정
-    @GetMapping("/company/info")
-    public String companyInfo() {
+    @GetMapping("/company/info/{id}")
+    public String companyInfo(@PathVariable int id, HttpServletRequest request) {
+        UserResponse.DetailDTO userDTO = userRepository.findByIdWithUser(id);
+        System.out.println(userRepository);
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
         return "company/companyInfo";
     }
 
     // 수정완료/취소 버튼 누르면 자원을 찾을 수 없음이라 나옴. 그것 수정하고 주석 지워주세요.
-    @GetMapping("/company/info/updateForm/{id}")
+    @GetMapping("/company/info/{id}/updateForm")
     public String companyInfoUpdateForm(@PathVariable int id, HttpServletRequest request) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         if (sessionUser == null) {
@@ -118,15 +124,15 @@ public class UserController {
             return "redirect:/company/loginForm";
         }
         User user = userRepository.findById(id);
-//        if (user.getId() != sessionUser.getId()){
-//            return "error/403";
-//        }
+        if (user.getId() != sessionUser.getId()){
+            return "error/403";
+        }
         request.setAttribute("user",user);
-        return "/company/updateInfoForm";
+        return "company/updateInfoForm";
     }
 
 //   여기에도 머스치에도 post를 적었는데 get이 나오는 이유가 무엇일까요.
-    @PostMapping("/company/info/update/{id}")
+    @PostMapping("/company/info/{id}/update")
     public String companyInfoUpdate(@PathVariable int id, UserRequest.UpdateDTO requestDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         if (sessionUser == null) {
@@ -139,7 +145,7 @@ public class UserController {
         userRepository.companyUpdate(requestDTO,id);
 
 
-        return "redirect:/company/info"+id;
+        return "redirect:/company/info/"+id;
     }
 
     //개인 프로필 정보 및 수정
--- miniproject1/src/main/java/com/many/miniproject1/user/UserResponse.java
+++ miniproject1/src/main/java/com/many/miniproject1/user/UserResponse.java
@@ -1,4 +1,21 @@
 package com.many.miniproject1.user;
 
+import lombok.Data;
+
 public class UserResponse {
+    @Data
+    public static class DetailDTO {
+        private Integer id;
+        private String role;
+        private String email;
+        private String password;
+        private String username;
+        private String tel;
+        private String companyName;
+        private String companyAddress;
+        private String companyNum;
+        private String profile;
+        private Boolean infoOwner;
+
+    }
 }
