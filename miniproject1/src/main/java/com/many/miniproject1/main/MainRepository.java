b7802bc1883e396cccd3e5cd670811c98822b731
JIYOUNG KONG <ij051277@gmail.com>
2024-03-05 09:51:06 +0900

    뷰수정, 채용공고 진행중임

--- miniproject1/src/main/java/com/many/miniproject1/main/MainRepository.java
+++ miniproject1/src/main/java/com/many/miniproject1/main/MainRepository.java
@@ -1,6 +1,8 @@
 package com.many.miniproject1.main;
 
+import com.many.miniproject1.post.Post;
 import com.many.miniproject1.resume.Resume;
+import com.many.miniproject1.user.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
@@ -13,5 +15,22 @@ import java.util.List;
 public class MainRepository {
     private final EntityManager em;
 
+    public List<User> findAll() {
+        Query query = em.createNativeQuery("select * from user_tb", User.class);
+
+        return query.getResultList();
+    }
+
+    public List<Post> findPost(int id) {
+        Query query = em.createNativeQuery("select * from post_tb where company_id=id", Post.class);
+
+        return query.getResultList();
+    }
+
+    public List<Resume> findResume(int id) {
+        Query query = em.createNativeQuery("select * from resume_tb where person_id=id", Resume.class);
+
+        return query.getResultList();
+    }
 
 }
