35a3e2b2947ca45ed3c97cdb9515d43f62e1990c
JIYOUNG KONG <ij051277@gmail.com>
2024-03-06 15:34:47 +0900

    스크랩하기 수정

--- src/main/java/com/many/miniproject1/apply/ApplyController.java
+++ src/main/java/com/many/miniproject1/apply/ApplyController.java
@@ -1,19 +1,47 @@
 package com.many.miniproject1.apply;
 
+import com.many.miniproject1.main.MainResponse;
+import com.many.miniproject1.post.Post;
+import com.many.miniproject1.skill.SkillRepository;
+import com.many.miniproject1.user.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class ApplyController {
-
+    private final HttpSession session;
+    private final ApplyRepository applyRepository;
+    private final SkillRepository skillRepository;
     //기업에서 받은 이력서 관리
 
     @GetMapping("/company/resume")
-    public String companyResume() {
+    public String companyResume(HttpServletRequest request) {
+        User sessionUser=(User) session.getAttribute("sessionUser");
+
+        List<Post> postList=applyRepository.findPost(sessionUser.getId());
+        System.out.println(postList.size());
+
+
+        ArrayList<MainResponse.postDTO> postSkillList=new ArrayList<>();
+        for(int i =0 ; i<postList.size(); i++){
+            List<String> skills=skillRepository.findByPostId(postList.get(i).getId());
+            System.out.println(skills);
+            Post post=(postList.get(i));
+            System.out.println(post);
+            postSkillList.add(new MainResponse.postDTO(post,skills));
+            System.out.println(postSkillList.get(i));
+        }
+        request.setAttribute("sessionuser", sessionUser);
+        request.setAttribute("postSkillList", postSkillList);
         return "company/companyResumes";
     }
 
