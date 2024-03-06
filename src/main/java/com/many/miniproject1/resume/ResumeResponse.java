55c3b6a38721f953669e038180f4e2273b0503de
khh <khh8500@naver.com>
2024-03-06 19:34:11 +0900

    ì스킬 추가

--- src/main/java/com/many/miniproject1/resume/ResumeResponse.java
+++ src/main/java/com/many/miniproject1/resume/ResumeResponse.java
@@ -7,8 +7,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ResumeResponse {
+    //    @Data
+//    public static class resumeDTO {
+//        private Integer id;
+//        private Integer personId;
+//        private String title;
+//        private String profile;
+//        private String portfolio;
+//        private String introduce;
+//        private String career;
+//        private String simpleIntroduce;
+//        private String email;
+//        private String password;
+//        private String username;
+//        private String tel;
+//        private String address;
+//        private Timestamp createdAt;
+//        private List<String> skill;
+//
+//        public resumeDTO(ResumeRequest.ResumeDTO, List<String> skill) {
+//            this.id = resume.getId();
+//            this.personId = resume.getPersonId();
+//            this.title = resume.getTitle();
+//            this.profile = resume.getProfile();
+//            this.portfolio = resume.getPortfolio();
+//            this.introduce = resume.getIntroduce();
+//            this.career = resume.getCareer();
+//            this.simpleIntroduce = resume.getSimpleIntroduce();
+//            this.createdAt = resume.getCreatedAt();
+//
+//            this.skill=skill;
+//        }
+//    }
+
     @Data
-    public static class resumeDTO {
+    public static class DetailSkillDTO {
         private Integer id;
         private Integer personId;
         private String title;
@@ -17,10 +50,15 @@ public class ResumeResponse {
         private String introduce;
         private String career;
         private String simpleIntroduce;
-        private List<String> skill;
+        private List<String> skills = new ArrayList<>();
         private Timestamp createdAt;
+        private String email;
+        private String username;
+        private String tel;
+        private String address;
+        private String birth;
 
-        public resumeDTO(Resume resume, List<String> skill) {
+        public DetailSkillDTO(DetailDTO resume, List<String> skills) {
             this.id = resume.getId();
             this.personId = resume.getPersonId();
             this.title = resume.getTitle();
@@ -30,8 +68,11 @@ public class ResumeResponse {
             this.career = resume.getCareer();
             this.simpleIntroduce = resume.getSimpleIntroduce();
             this.createdAt = resume.getCreatedAt();
-
-            this.skill=skill;
+            this.email = resume.getEmail();
+            this.username = resume.getUsername();
+            this.tel = resume.getTel();
+            this.address = resume.getAddress();
+            this.birth = resume.getBirth();
         }
     }
 
@@ -45,24 +86,14 @@ public class ResumeResponse {
         private String introduce;
         private String career;
         private String simpleIntroduce;
-        private List<String> skill = new ArrayList<>();
         private Timestamp createdAt;
-
-        public DetailDTO(Resume resume, List<String> skill) {
-            this.id = resume.getId();
-            this.personId = resume.getPersonId();
-            this.title = resume.getTitle();
-            this.profile = resume.getProfile();
-            this.portfolio = resume.getPortfolio();
-            this.introduce = resume.getIntroduce();
-            this.career = resume.getCareer();
-            this.simpleIntroduce = resume.getSimpleIntroduce();
-            this.createdAt = resume.getCreatedAt();
-        }
-
-        public DetailDTO() {
-        }
+        private String email;
+        private String username;
+        private String tel;
+        private String address;
+        private String birth;
     }
+
     @Data
     public static class UpdateDTO {
         private Integer id;
