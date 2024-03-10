bacdec8713092dc9d2e8014eaaae1b8f12210e0e
jiyoung <ij0512@naver.com>
2024-03-10 10:00:22 +0900

    업데이트

--- src/main/java/com/many/miniproject1/main/MainRepository.java
+++ src/main/java/com/many/miniproject1/main/MainRepository.java
@@ -1,6 +1,7 @@
 package com.many.miniproject1.main;
 
 import com.many.miniproject1.post.Post;
+import com.many.miniproject1.post.PostResponse;
 import com.many.miniproject1.resume.Resume;
 import com.many.miniproject1.resume.ResumeResponse;
 import com.many.miniproject1.user.User;
@@ -96,9 +97,9 @@ public class MainRepository {
         return result;
     }
 
-    public ResumeResponse.DetailDTO findMainResume(int idx) {
+    public ResumeResponse.DetailDTO findMainResume(int resumeId) {
         Query query = em.createNativeQuery("SELECT u.email, u.username, u.tel, u.address, u.birth, r.id, r.person_id, r.title, r.profile, r.portfolio, r.introduce, r.career, r.simple_introduce, r.created_at FROM user_tb u INNER JOIN resume_tb r ON u.id=r.person_id where r.id = ? ");
-        query.setParameter(1, idx);
+        query.setParameter(1, resumeId);
 
         Object[] row = (Object[]) query.getSingleResult();
 
@@ -137,4 +138,38 @@ public class MainRepository {
         return responseDTO;
     }
 
+    public Post findMainPost(int postId) {
+            Query query = em.createNativeQuery("select p.id, p.title, p.career, p.pay, p.work_condition, p.work_start_time, p.work_end_time, p.deadline, p.task, p.profile, p.working_area from post_tb p inner join user_tb u on p.company_id = u.id where p.id=?");
+            query.setParameter(1, postId);
+
+            Object[] row = (Object[]) query.getSingleResult();
+
+            Integer id = (Integer) row[0];
+            String title = (String) row[1];
+            String career = (String) row[2];
+            String pay = (String) row[3];
+            String workCondition = (String) row[4];
+            String workStartTime = (String) row[5];
+            String workEndTime = (String) row[6];
+            String deadline = (String) row[7];
+            String task = (String) row[8];
+            String profile = (String) row[9];
+            String workingArea = (String) row[10];
+
+            //객체를 생성하여서 DetailDTO로 옮겨담는 작업
+            Post responseDTO = new Post();
+            responseDTO.setId(id);
+            responseDTO.setTitle(title);
+            responseDTO.setCareer(career);
+            responseDTO.setPay(pay);
+            responseDTO.setWorkCondition(workCondition);
+            responseDTO.setWorkStartTime(workStartTime);
+            responseDTO.setWorkEndTime(workEndTime);
+            responseDTO.setDeadline(deadline);
+            responseDTO.setTask(task);
+            responseDTO.setProfile(profile);
+            responseDTO.setWorkingArea(workingArea);
+
+            return responseDTO;
+        }
 }
