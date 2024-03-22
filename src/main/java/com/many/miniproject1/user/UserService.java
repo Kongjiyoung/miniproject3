28034e73c671e57017a8ca555adbf31629ec6b03
khh <khh8500@naver.com>
2024-03-23 02:04:48 +0900

    Service-회원수정

--- src/main/java/com/many/miniproject1/user/UserService.java
+++ src/main/java/com/many/miniproject1/user/UserService.java
@@ -2,6 +2,7 @@ package com.many.miniproject1.user;
 
 import com.many.miniproject1._core.errors.exception.Exception401;
 import com.many.miniproject1._core.errors.exception.Exception404;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     public final UserJPARepository userJPARepository;
+
+    @Transactional
+    public User 회원수정(int id, UserRequest.CompanyInfoUpdateDTO requestDTO){
+        User user = userJPARepository.findById(id)
+                .orElseThrow(() -> new Exception404("회원정보를 찾을 수 없습니다"));
+
+        user.setProfile(requestDTO.getProfile);
+        user.setAddress(requestDTO.getAddress());
+        user.setTel(requestDTO.getTel());
+        user.setEmail(requestDTO.getEmail());
+        user.setPassword(requestDTO.getPassword());
+        return user;
+    }
 
     public User 회원조회(int id){
         User user = userJPARepository.findById(id)
