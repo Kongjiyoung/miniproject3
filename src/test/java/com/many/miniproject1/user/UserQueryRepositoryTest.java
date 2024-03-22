a69c0f17ebddcfcc93d16540a3cbf5c4a721d199
hyeonnit <lhyeonnit@gmail.com>
2024-03-22 16:54:41 +0900

    findById_test

--- src/test/java/com/many/miniproject1/user/UserQueryRepositoryTest.java
+++ src/test/java/com/many/miniproject1/user/UserQueryRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.many.miniproject1.user;
 
 import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -12,4 +14,16 @@ public class UserQueryRepositoryTest {
     UserQueryRepository userQueryRepository;
     @Autowired
     private EntityManager em;
+
+    @Test
+        public void findById_test(){
+            // given
+        int id = 1;
+            // when
+        User user = userQueryRepository.findById(id);
+        System.out.println("findById_test: "+ user);
+            // then
+        Assertions.assertThat(user.getUsername()).isEqualTo("captain_kong");
+        Assertions.assertThat(user.getName()).isEqualTo("공지영");
+        }
 }
