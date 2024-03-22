80579108fba7c82977363a9e53cfe905b1cc773d
khh <khh8500@naver.com>
2024-03-23 03:16:42 +0900

    Service수정-이미지 오류 해결/이미지, 비밀번호 수정 안됨

--- src/main/java/com/many/miniproject1/user/UserService.java
+++ src/main/java/com/many/miniproject1/user/UserService.java
@@ -2,9 +2,12 @@ package com.many.miniproject1.user;
 
 import com.many.miniproject1._core.errors.exception.Exception401;
 import com.many.miniproject1._core.errors.exception.Exception404;
+import io.micrometer.common.util.StringUtils;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
 
 @RequiredArgsConstructor
 @Service
@@ -16,7 +19,13 @@ public class UserService {
         User user = userJPARepository.findById(id)
                 .orElseThrow(() -> new Exception404("회원정보를 찾을 수 없습니다"));
 
-        user.setProfile(requestDTO.getProfile());
+        if (!StringUtils.isEmpty(requestDTO.getProfileBase64())) {
+            // Base64로 인코딩된 이미지 문자열을 디코딩하여 바이트 배열로 변환
+            byte[] profileImageBytes = Base64.getDecoder().decode(requestDTO.getProfileBase64());
+            String profileImageBase64 = Base64.getEncoder().encodeToString(profileImageBytes); // 다시 Base64로 인코딩 (원하는 경우)
+            user.setProfile(profileImageBase64); // 이미지를 Base64로 인코딩한 문자열로 저장
+        }
+
         user.setAddress(requestDTO.getAddress());
         user.setTel(requestDTO.getTel());
         user.setEmail(requestDTO.getEmail());
