f088681b241a9d1d207fda50439d2dcfb1358d68
khh <khh8500@naver.com>
2024-03-23 01:36:02 +0900

    ㅕuserUpdateDTO

--- src/main/java/com/many/miniproject1/user/UserRequest.java
+++ src/main/java/com/many/miniproject1/user/UserRequest.java
@@ -5,6 +5,14 @@ import lombok.Data;
 public class UserRequest {
 
     @Data
+    public static class UpdateDTO {
+        private String address;
+        private String tel;
+        private String email;
+        private String password;
+    }
+
+    @Data
     public static class LoginDTO{
         private String username;
         private String password;
