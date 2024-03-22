320df0d229b8e33d17484d536044be402593d683
Kongjiyoung <ij0512@naver.com>
2024-03-22 19:46:54 +0900

    findByUsernameAndPassword_test

--- src/main/java/com/many/miniproject1/user/UserJPARepository.java
+++ src/main/java/com/many/miniproject1/user/UserJPARepository.java
@@ -3,8 +3,9 @@ package com.many.miniproject1.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface UserJPARepository extends JpaRepository<User, Integer> {
 
     User findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
-
 }
--- src/test/java/com/many/miniproject1/user/UserJPARepositoryTest.java
+++ src/test/java/com/many/miniproject1/user/UserJPARepositoryTest.java
@@ -1,27 +1,29 @@
 package com.many.miniproject1.user;
 
 import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-@Import(UserJPARepository.class)
+//@Import(UserJPARepository.class)
 @DataJpaTest
 public class UserJPARepositoryTest {
     @Autowired
-    UserJPARepository userJPARepository;
+    private UserJPARepository userJPARepository;
     @Autowired
     private EntityManager em;
 
     @Test
-    public void findByUsernameAndPassword_test(){
+    public void findByUsernameAndPassword_test() {
         // given
-
-
+        String username = "captain_kong";
+        String password = "1234";
         // when
-
-
+        User user = userJPARepository.findByUsernameAndPassword(username, password);
+        System.out.println("findByUsernameAndPassword_test: " + user);
         // then
+        Assertions.assertThat(user.getAddress()).isEqualTo("부산광역시");
     }
 }
