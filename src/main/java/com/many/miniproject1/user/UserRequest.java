6f452652eb4b7eee42054a013ce6ad3bb9b14606
hyeonn <lhyeonnit@gmail.com>
2024-03-23 12:31:30 +0900

    PersonUpdateDTO

--- src/main/java/com/many/miniproject1/user/UserRequest.java
+++ src/main/java/com/many/miniproject1/user/UserRequest.java
@@ -1,8 +1,33 @@
 package com.many.miniproject1.user;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.sql.Date;
 
 public class UserRequest {
+
+    @Data
+    public static class PersonUpdateDTO{
+        private String email;
+        private String name;
+        private String password;
+        private String tel;
+        private String address;
+        private Date birth;
+        private MultipartFile profile;
+        public User toEntity(){
+            return User.builder()
+                    .email(email)
+                    .name(name)
+                    .password(password)
+                    .tel(tel)
+                    .address(address)
+                    .birth(birth)
+                    .profile(profile.getOriginalFilename())
+                    .build();
+        }
+    }
 
     @Data
     public static class CompanyInfoUpdateDTO {
