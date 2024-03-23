88995e53b4dff5c346e29acce78d07237d5060a8
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-03-23 10:56:12 +0900

    person 회원가입, 폼 수정 완료

--- src/main/java/com/many/miniproject1/user/UserService.java
+++ src/main/java/com/many/miniproject1/user/UserService.java
@@ -6,8 +6,14 @@ import io.micrometer.common.util.StringUtils;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Base64;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -15,7 +21,18 @@ public class UserService {
     public final UserJPARepository userJPARepository;
 
     @Transactional
-    public User 회원수정(int id, UserRequest.CompanyInfoUpdateDTO requestDTO){
+    public void personJoin(UserRequest.PersonJoinDTO reqDTO) {
+        // 이미지 저장
+        String profileFilename = ProfileImageService.saveProfile(reqDTO.getProfile());
+
+        // 사용자 정보 저장
+        User user = reqDTO.toEntity();
+        user.setProfile(profileFilename);
+        userJPARepository.save(user);
+    }
+
+    @Transactional
+    public User 회원수정(int id, UserRequest.CompanyInfoUpdateDTO requestDTO) {
         User user = userJPARepository.findById(id)
                 .orElseThrow(() -> new Exception404("회원정보를 찾을 수 없습니다"));
 
@@ -33,13 +50,13 @@ public class UserService {
         return user;
     }
 
-    public User 회원조회(int id){
+    public User 회원조회(int id) {
         User user = userJPARepository.findById(id)
                 .orElseThrow(() -> new Exception404("회원정보를 찾을 수 없습니다"));
         return user;
     }
 
-    public User 로그인(UserRequest.LoginDTO reqestDTO){
+    public User 로그인(UserRequest.LoginDTO reqestDTO) {
         User sessionUser = userJPARepository.findByUsernameAndPassword(reqestDTO.getUsername(), reqestDTO.getPassword())
                 .orElseThrow(() -> new Exception401("인증되지 않았습니다"));
         return sessionUser;
