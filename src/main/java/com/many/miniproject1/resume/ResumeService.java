82cdd424d646f0ce55e2ac53232368a654dd092a
jiyoung <ij0512@naver.com>
2024-03-24 20:28:16 +0900

    잘못만듬

--- src/main/java/com/many/miniproject1/resume/ResumeService.java
+++ src/main/java/com/many/miniproject1/resume/ResumeService.java
@@ -2,6 +2,7 @@ package com.many.miniproject1.resume;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RequiredArgsConstructor
@@ -9,4 +10,10 @@ import org.springframework.stereotype.Service;
 public class ResumeService {
     private final ResumeJPARepository resumeJPARepository;
     private final ResumeQueryRepository resumeQueryRepository;
+
+    @Transactional
+    public Resume save(ResumeRequest.SaveDTO requestDTO) {
+
+        return resume;
+    }
 }
