73a2766d2645691b61d89bee1f8fed2f48e1f305
hyeonnit <lhyeonnit@gmail.com>
2024-03-25 15:42:09 +0900

    ResumeJPARepository findByIdJoinUser

--- src/main/java/com/many/miniproject1/resume/ResumeJPARepository.java
+++ src/main/java/com/many/miniproject1/resume/ResumeJPARepository.java
@@ -2,10 +2,13 @@ package com.many.miniproject1.resume;
 
 import com.many.miniproject1.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ResumeJPARepository extends JpaRepository<Resume, Integer> {
 
+    @Query("select r from Resume r join fetch r.user u where r.id = :id")
+    Optional<Resume> findByIdJoinUser(@Param("id") int id);
 }
