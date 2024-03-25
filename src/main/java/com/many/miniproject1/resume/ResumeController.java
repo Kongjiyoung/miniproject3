d5a4006d915e2f6c080a187e8f3be8e0fdf22a7a
khh <khh8500@naver.com>
2024-03-25 16:55:16 +0900

    Controller-update

--- src/main/java/com/many/miniproject1/resume/ResumeController.java
+++ src/main/java/com/many/miniproject1/resume/ResumeController.java
@@ -1,6 +1,9 @@
 package com.many.miniproject1.resume;
 
+import com.many.miniproject1._core.errors.exception.Exception404;
+import com.many.miniproject1.user.User;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ResumeController{
     private final ResumeService resumeService;
+    private final HttpSession session;
 
     //개인 이력서 관리
     @GetMapping("/person/resume")
@@ -37,14 +41,17 @@ public class ResumeController{
     }
 
     @GetMapping("/person/resume/detail/{id}/update-form")
-    public String personUpdateResumeForm(@PathVariable int id, HttpServletRequest request) {
+    public String personUpdateResumeForm(HttpServletRequest request) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        Resume resume = resumeService.findByUser(sessionUser.getId());
+        request.setAttribute("resume", resume);
         return "person/resume-update-form";
     }
 
     @PostMapping("/person/resume/{id}/detail/update")
-    public String personUpdateResume(@PathVariable int id, ResumeRequest.UpdateDTO requestDTO, HttpServletRequest request, @RequestParam("skill") List<String> skills) {
+    public String personUpdateResume(@PathVariable int id, ResumeRequest.UpdateDTO requestDTO) {
+        Resume updatedResume = resumeService.update(id, requestDTO);
         return "redirect:/person/resume/" + id + "/detail";
-
     }
 
     @PostMapping("/person/resume/detail/{id}/delete")
