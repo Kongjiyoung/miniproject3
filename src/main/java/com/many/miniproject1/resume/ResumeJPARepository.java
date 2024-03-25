d5ca5c0e1e57702a549a85b70b3de8c0484f54ee
khh <khh8500@naver.com>
2024-03-25 14:52:45 +0900

    복구

--- src/main/java/com/many/miniproject1/resume/ResumeJPARepository.java
+++ src/main/java/com/many/miniproject1/resume/ResumeJPARepository.java
@@ -1,10 +1,6 @@
 package com.many.miniproject1.resume;
 
-import com.many.miniproject1.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface ResumeJPARepository extends JpaRepository<Resume, Integer> {
 
