9f0330afc9047836d5b6a62f6a03cb75d1722ff6
hyeonnit <lhyeonnit@gmail.com>
2024-03-25 15:48:45 +0900

    findByIdJoinUser_test

--- src/test/java/com/many/miniproject1/resume/ResumeJPARepositoryTest.java
+++ src/test/java/com/many/miniproject1/resume/ResumeJPARepositoryTest.java
@@ -6,8 +6,10 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Sort;
 
 import java.sql.Date;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,4 +22,27 @@ public class ResumeJPARepositoryTest {
     @Autowired
     private EntityManager em;
 
+    @Test
+    public void findByIdJoinUser_test() {
+        // given
+        int id = 1;
+        // when
+        Resume resume = resumeJPARepository.findByIdJoinUser(id).get();
+        // then
+        System.out.println("findByIdJoinUser_test: " + resume.getUser().getName());
+        System.out.println("findByIdJoinUser_test: " + resume.getTitle());
+    }
+
+    @Test
+    public void findById_test() {
+        // given
+        int id = 1;
+        // when
+        Optional<Resume> resumeOP = resumeJPARepository.findById(id);
+        if (resumeOP.isPresent()) {
+            Resume resume = resumeOP.get();
+            System.out.println("findById_test: " + resume.getTitle());
+        }
+        // then
+    }
 }
