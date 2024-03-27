cb0d3fcafc96fe12816153ad549fb13ac5673917
khh <khh8500@naver.com>
2024-03-27 15:46:14 +0900

    Service-delete

--- src/main/java/com/many/miniproject1/post/PostService.java
+++ src/main/java/com/many/miniproject1/post/PostService.java
@@ -1,7 +1,10 @@
 package com.many.miniproject1.post;
 
+import com.many.miniproject1._core.errors.exception.Exception403;
+import com.many.miniproject1._core.errors.exception.Exception404;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @RequiredArgsConstructor
@@ -9,4 +12,16 @@ import org.springframework.stereotype.Service;
 public class PostService {
     private final PostJPARepository postJPARepository;
     private final PostQueryRepository postQueryRepository;
+
+    @Transactional
+    public void postDelete(int postId, int sessionUserId) {
+        Post post = postJPARepository.findById(postId)
+                .orElseThrow(() -> new Exception404("공고글을 찾을 수 없습니다"));
+
+        if(sessionUserId != post.getUser().getId()){
+            throw new Exception403("공고글을 삭제할 권한이 없습니다");
+        }
+
+        postJPARepository.deleteById(postId);
+    }
 }
