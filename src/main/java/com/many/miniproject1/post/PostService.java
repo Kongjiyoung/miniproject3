8a1eee9b2d5a2d95b0082ce7320a5893aa6eea7a
hyeonnit <lhyeonnit@gmail.com>
2024-03-27 11:29:33 +0900

    postService save

--- src/main/java/com/many/miniproject1/post/PostService.java
+++ src/main/java/com/many/miniproject1/post/PostService.java
@@ -1,5 +1,7 @@
 package com.many.miniproject1.post;
 
+import com.many.miniproject1.user.User;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,9 @@ import org.springframework.stereotype.Service;
 public class PostService {
     private final PostJPARepository postJPARepository;
     private final PostQueryRepository postQueryRepository;
+
+    @Transactional
+    public void save(PostRequest.PostSaveDTO reqDTO, User sessionUser){
+        postJPARepository.save(reqDTO.toEntity(sessionUser));
+    }
 }
