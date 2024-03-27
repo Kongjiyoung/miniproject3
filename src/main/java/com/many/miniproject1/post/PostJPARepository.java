d5912519dde31213aa5d2f01a35b81a007339c7b
Kongjiyoung <ij0512@naver.com>
2024-03-27 13:48:05 +0900

    repository 작성, ãservice 작성

--- src/main/java/com/many/miniproject1/post/PostJPARepository.java
+++ src/main/java/com/many/miniproject1/post/PostJPARepository.java
@@ -1,7 +1,15 @@
 package com.many.miniproject1.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostJPARepository extends JpaRepository<Post, Integer> {
 
+    @Query("""
+            select p from Post p join fetch p.skillList s join fetch p.user u where u.id= :id
+            """)
+    List<Post> findByUserIdJoinSkillAndUser(@Param("id") int id);
 }
--- src/main/java/com/many/miniproject1/post/PostService.java
+++ src/main/java/com/many/miniproject1/post/PostService.java
@@ -3,10 +3,14 @@ package com.many.miniproject1.post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @RequiredArgsConstructor
 @Service
 public class PostService {
     private final PostJPARepository postJPARepository;
     private final PostQueryRepository postQueryRepository;
+
+    public List<Post> getResumeList()
 }
