2787cb3cb5eeaff7c6d3a6e2d534433140ab3564
Kongjiyoung <ij0512@naver.com>
2024-03-27 13:53:03 +0900

    controller 작성

--- src/main/java/com/many/miniproject1/post/PostService.java
+++ src/main/java/com/many/miniproject1/post/PostService.java
@@ -11,7 +11,6 @@ import java.util.List;
 public class PostService {
     private final PostJPARepository postJPARepository;
     private final PostQueryRepository postQueryRepository;
-
     public List<Post> getResumeList(Integer userId){
         return postJPARepository.findByUserIdJoinSkillAndUser(userId);
     }
