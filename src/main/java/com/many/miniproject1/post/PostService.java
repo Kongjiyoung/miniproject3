232f23424dbc9509a77e0ac3d52dad486f961b28
hyeonnit <lhyeonnit@gmail.com>
2024-03-27 11:48:08 +0900

    일단 skill 추가

--- src/main/java/com/many/miniproject1/post/PostService.java
+++ src/main/java/com/many/miniproject1/post/PostService.java
@@ -1,9 +1,16 @@
 package com.many.miniproject1.post;
 
+import com.many.miniproject1.resume.Resume;
+import com.many.miniproject1.skill.Skill;
+import com.many.miniproject1.skill.SkillJPARepository;
+import com.many.miniproject1.skill.SkillResponse;
 import com.many.miniproject1.user.User;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -11,9 +18,19 @@ import org.springframework.stereotype.Service;
 public class PostService {
     private final PostJPARepository postJPARepository;
     private final PostQueryRepository postQueryRepository;
+    private final SkillJPARepository skillJPARepository;
 
     @Transactional
-    public void save(PostRequest.PostSaveDTO reqDTO, User sessionUser){
-        postJPARepository.save(reqDTO.toEntity(sessionUser));
+    public Post save(PostRequest.PostSaveDTO reqDTO, User sessionUser){
+        Post post = postJPARepository.save(reqDTO.toEntity(sessionUser));
+
+        List<Skill> skills = new ArrayList<>();
+        for (String skillName : reqDTO.getSkill()) {
+            SkillResponse.SaveDTO skill = new SkillResponse.SaveDTO();
+            skill.setSkill(skillName);
+            skills.add(skill.toEntity());
+        }
+        skillJPARepository.saveAll(skills);
+        return post;
     }
 }
