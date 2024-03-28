83acbe7701436d76b4b3abb731e2a2992fac1aeb
Kongjiyoung <ij0512@naver.com>
2024-03-28 11:18:24 +0900

    JPARepository 작성

--- src/main/java/com/many/miniproject1/apply/ApplyJPARepository.java
+++ src/main/java/com/many/miniproject1/apply/ApplyJPARepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ApplyJPARepository extends JpaRepository<Apply, Integer> {
 
-
+    @Query("""
+            select distinct a from Apply a join fetch a.post p join fetch p.user u join fetch p.skillList s where p.id= :id
+            """)
+    Optional<Apply> findByPostIdJoinPostAndSkillAndUser(@Param("id") int id);
 }
