ec4a352425451104a46685937331326dcbc80848
khh <khh8500@naver.com>
2024-03-28 11:11:29 +0900

    Service-companyResumes

--- src/main/java/com/many/miniproject1/apply/ApplyService.java
+++ src/main/java/com/many/miniproject1/apply/ApplyService.java
@@ -4,7 +4,10 @@ package com.many.miniproject1.apply;
 import com.many.miniproject1.skill.SkillJPARepository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +16,8 @@ public class ApplyService {
     private final ApplyQueryRepository applyQueryRepository;
     private final SkillJPARepository skillJPARepository;
 
-
+    public List<Apply> companyResumes() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        return applyJPARepository.findAll(sort);
+    }
 }
