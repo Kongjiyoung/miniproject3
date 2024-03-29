de8f98357f9f5d7482d4f27309a130a03c0ccb10
khh <khh8500@naver.com>
2024-03-29 13:57:57 +0900

    Repository-personScrap

--- src/main/java/com/many/miniproject1/scrap/ScrapJPARepository.java
+++ src/main/java/com/many/miniproject1/scrap/ScrapJPARepository.java
@@ -1,9 +1,12 @@
 package com.many.miniproject1.scrap;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ScrapJPARepository extends JpaRepository<Scrap, Integer> {
 
@@ -11,4 +14,12 @@ public interface ScrapJPARepository extends JpaRepository<Scrap, Integer> {
 //    @Query("delete from Scrap s where s.post.id = :post_id and s.resume.user.id= :resume_user_id")
 //    void deleteScrapByPostId(@Param("post_id") Integer postId, @Param("resume_user_id") Integer resumeUserId);
 
+    @Query("""
+            select s
+            from Scrap s
+            join fetch s.post p
+            join fetch p.skillList l
+            where s.resume.user.id = :userId
+            """)
+    List<Scrap> findByPostIdJoinskills(@Param("user_id") Integer userId);
 }
