e0cb57c249443312c0e98ec1a699f4849da172df
hyeonnit <lhyeonnit@gmail.com>
2024-03-29 11:40:16 +0900

    ScrapJPARepository findByUserIdJoinSkill

--- src/main/java/com/many/miniproject1/scrap/ScrapJPARepository.java
+++ src/main/java/com/many/miniproject1/scrap/ScrapJPARepository.java
@@ -5,10 +5,22 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ScrapJPARepository extends JpaRepository<Scrap, Integer> {
 
 //    @Modifying
 //    @Query("delete from Scrap s where s.post.id = :post_id and s.resume.user.id= :resume_user_id")
 //    void deleteScrapByPostId(@Param("post_id") Integer postId, @Param("resume_user_id") Integer resumeUserId);
+
+    @Query("""
+            select distinct s
+            from Scrap s
+            JOIN FETCH s.resume r
+            JOIN FETCH r.skillList rs
+            join FETCH r.user ru
+            where ru.id = :user_id
+            """)
+    List<Scrap> findByUserIdJoinSkill (@Param("user_id") Integer userId);
 
 }
