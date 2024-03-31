485c6a4375c080f9d0f237abf60b27733dbf921e
LifeIsOne <didtmdgh258@gmail.com>
2024-03-31 16:48:31 +0900

    Post Controller Edit

--- src/main/java/com/many/miniproject1/post/PostController.java
+++ src/main/java/com/many/miniproject1/post/PostController.java
@@ -1,18 +1,18 @@
 package com.many.miniproject1.post;
 
+import com.many.miniproject1._core.utils.ApiUtil;
 import com.many.miniproject1.skill.Skill;
 import com.many.miniproject1.user.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class PostController {
     private final HttpSession session;
@@ -20,55 +20,42 @@ public class PostController {
 
     //회사 공고 관리
     @GetMapping("/company/posts")
-    public String companyPosts(HttpServletRequest request) { // 이 페이지는 포스트들을 확인할 수 있는 페이지라 이름 변경했습니다.
+    public ResponseEntity<?> companyPosts(HttpServletRequest request) { // 이 페이지는 포스트들을 확인할 수 있는 페이지라 이름 변경했습니다.
         User sessionUser = (User) session.getAttribute("sessionUser");
         List<Post> postList =postService.getResumeList(sessionUser.getId());
-        request.setAttribute("postlist", postList);
-        return "company/posts";
+
+        return ResponseEntity.ok(new ApiUtil<>(postList));
     }
 
-    // 공고 상세보기 YSH
     @GetMapping("/company/posts/{id}")
-    public String companyPostDetailForm(HttpServletRequest request, @PathVariable Integer id) {
+    public ResponseEntity<?> companyPostDetailForm(HttpServletRequest request, @PathVariable Integer id) {
         Post post = postService.postDetail(id);
         request.setAttribute("post",post);
-        return "company/post-detail";
+        return ResponseEntity.ok(new ApiUtil<>(post));
     }
 
-    @GetMapping("/company/posts/save-form")
-    public String companyPostForm() {
-        return "company/post-save-form";
-    }
-
+    // 이력서 저장
     @PostMapping("/company/posts/save")
-    public String companySavePost(PostRequest.PostSaveDTO reqDTO) {
+    public ResponseEntity<?> companySavePost(PostRequest.PostSaveDTO reqDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         postService.save(reqDTO, sessionUser);
-        return "redirect:/company/posts";
+        return ResponseEntity.ok(new ApiUtil<>(reqDTO));
     }
 
-    @GetMapping("/company/posts/{id}/update-form")
-    public String companyUpdatePostForm(@PathVariable int id, HttpServletRequest request) {
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        Post post = postService.findByPost(id);
-        request.setAttribute("post", post);
-
-        return "company/post-update-form";
-    }
 
     @PostMapping("/company/posts/{id}/update")
-    public String companyUpdatePost(@PathVariable int id, PostRequest.UpdatePostDTO reqDTO) {
+    public ResponseEntity<?> companyUpdatePost(@PathVariable int id, PostRequest.UpdatePostDTO reqDTO) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         System.out.println(reqDTO);
         postService.updatePost(id, sessionUser.getId(), reqDTO);
 
-        return "redirect:/company/posts/" + id;
+        return ResponseEntity.ok(new ApiUtil<>(reqDTO));
     }
 
-    @PostMapping("/company/posts/{id}/delete")
-    public String companyDeletePost(@PathVariable int id) {
+    @DeleteMapping("/company/posts/{id}/delete")
+    public ResponseEntity<?> companyDeletePost(@PathVariable int id) {
         User sessionUser = (User) session.getAttribute("sessionUser");
         postService.postDelete(id, sessionUser.getId());
-        return "redirect:/company/posts";
+        return ResponseEntity.ok(new ApiUtil<>(null));
     }
 }
