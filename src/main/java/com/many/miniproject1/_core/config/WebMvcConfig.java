f05b5bec482caf5f39aa439835b5dbf56306d99e
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-03-31 10:35:48 +0900

    컨피그 파일 수정

--- src/main/java/com/many/miniproject1/_core/config/WebMvcConfig.java
+++ src/main/java/com/many/miniproject1/_core/config/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package com.many.miniproject1._core.config;
 
+import com.many.miniproject1._core.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
@@ -8,13 +10,13 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 @Configuration // IoC
 public class WebMvcConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new LoginInterceptor())
-//                .addPathPatterns("/board/**", "/user/**")
-//                .excludePathPatterns("/board/{id:\\d+}");
-//
-//    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/board/{id:\\d+}");
+
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
