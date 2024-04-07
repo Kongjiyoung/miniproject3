478b17f3108cbfa1f4e7398051d2777211aad8c0
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-04-07 18:43:19 +0900

    offer 포스트맨 동기화 중 비어있는 바디 발견

--- src/main/java/com/many/miniproject1/offer/OfferController.java
+++ src/main/java/com/many/miniproject1/offer/OfferController.java
@@ -19,42 +19,26 @@ public class OfferController {
     private final ScrapService scrapService;
     private final MainService mainService;
 
-    // person의 offers 관리
-    @GetMapping("/api/person/my-page/offers")
-    public ResponseEntity<?> personOffers() {
-        SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
-        List<OfferResponse.PersonOffersDTO> respDTO = offerService.personOffers(sessionUser.getId());
-
-        return ResponseEntity.ok(new ApiUtil<>(respDTO));
-    }
-
-    // person의 offer 상세보기
-    @GetMapping("/api/person/my-page/offers/{id}")
-    public ResponseEntity<?> personOfferDetail(@PathVariable Integer id) {
-        OfferResponse.PersonOfferDetailDTO respDTO = offerService.personOfferDetail(id);
-
-        return ResponseEntity.ok(new ApiUtil<>(respDTO));
-    }
-
-    // company의 offers 관리
+    /////////////// 기업(오퍼를 보내는 입장)
+    // 개인의 이력서로 보낸 제안 목록(공고 목록이 보임)
     @GetMapping("/api/company/my-page/offers")
     public ResponseEntity<?> companyOffers() {
         SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
-        List<OfferResponse.CompanyOffersDTO> respDTO = offerService.companyOffers(14);
+        List<OfferResponse.CompanyOffersDTO> respDTO = offerService.companyOffers(sessionUser.getId());
 
         return ResponseEntity.ok(new ApiUtil<>(respDTO));
     }
 
     // 04-02
-    // company의 offer 상세보기
+    // 개인의 이력서로 보낸 제안 디테일(공고 디테일이 보임)
     @GetMapping("/api/company/my-page/offers/{id}")
-    public ResponseEntity<?> companyOfferDetail(@PathVariable int id) {
+    public ResponseEntity<?> companyOfferDetail(@PathVariable Integer id) {
         OfferResponse.CompanyOfferDetailDTO respDTO = offerService.companyOfferDetail(id);
 
         return ResponseEntity.ok(new ApiUtil<>(respDTO));
     }
 
-    // company의 offer DELETE (취소)
+    // 제안 철회하기
     @DeleteMapping("/api/company/my-page/offers/{id}")
     public ResponseEntity<?> companyOfferDetailDelete(@PathVariable Integer id) {
 //        User sessionUser = (User) session.getAttribute("sessionUser");
@@ -63,18 +47,36 @@ public class OfferController {
         return ResponseEntity.ok(new ApiUtil<>(null));
     }
 
-    // 메인에서 제안하기
+    // 메인 화면에 게시된 이력서에 제안하기
     @PostMapping("/api/main/company/resumes/{id}/offer")
     public ResponseEntity<?> companyResumeOffer(@PathVariable Integer id,@RequestBody OfferRequest.PostChoiceDTO reqDTO) {
         OfferResponse.OfferDTO respDTO = offerService.offerInMain(id, reqDTO.getPostChoice());
-        
+
         return ResponseEntity.ok(new ApiUtil<>(respDTO));
     }
 
-    // 스크랩에서 제아하기
+    // 스크랩한 이력서에 제안하기
     @PostMapping("/api/company/my-page/scraps/{id}/offer")
     public ResponseEntity<?> companyScrapResume(@PathVariable Integer id,@RequestBody OfferRequest.PostChoiceDTO reqDTO) {
         OfferResponse.OfferDTO respDTO = offerService.offerInScrap(id, reqDTO.getPostChoice());
+
+        return ResponseEntity.ok(new ApiUtil<>(respDTO));
+    }
+
+    /////////////// 개인(오퍼를 받는 입장)
+    // 내 이력서로 온 제안 목록(공고 목록)
+    @GetMapping("/api/person/my-page/offers")
+    public ResponseEntity<?> personOffers() {
+        SessionUser sessionUser = (SessionUser) session.getAttribute("sessionUser");
+        List<OfferResponse.PersonOffersDTO> respDTO = offerService.personOffers(sessionUser.getId());
+
+        return ResponseEntity.ok(new ApiUtil<>(respDTO));
+    }
+
+    // 내 이력서로 온 제안 디테일(공고 디테일)
+    @GetMapping("/api/person/my-page/offers/{id}")
+    public ResponseEntity<?> personOfferDetail(@PathVariable Integer id) {
+        OfferResponse.PersonOfferDetailDTO respDTO = offerService.personOfferDetail(id);
 
         return ResponseEntity.ok(new ApiUtil<>(respDTO));
     }
